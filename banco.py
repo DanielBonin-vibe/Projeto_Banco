1a72f4068e55bf7d809c6d879dfd7ca2f30fee81
Daniel Bonin <danielbonin0011@gmail.com>
2026-08-10 15:48:59 -0300

    Criada as tabelas, partes do JOIns e parte do cadastro de conat e cliente, faremos uma refatoração completa do main.py, afimd e torna-lo mais corretoe  comprensível

--- banco.py
+++ banco.py
@@ -1,21 +1,32 @@
 from cliente import Cliente
 from conta import Conta
+from utils import banco_de_dados
 
 
 class Banco:
 
 ##################################################
 # Cliente
-    def cadastrar_cliente(self, nome, idade, cpf, saldo):
-        ...
-    def remover_cliente(self): 
-        ...
+    def cadastrar_cliente(self, nome, idade, cpf):
+        banco_de_dados.cadastro_cliente(nome, idade, cpf)
+        print('Cliente cadastrado, siga com a abertura da conta')
+    def remover_cliente(self, id_cliente): 
+        banco_de_dados.remover_cadastro(id_cliente)
+
     def buscar_cliente(self, buscar_cpf):
         ...
     def listar_clientes(self):
         ...
+
+
 ##################################################
 # Conta
+
+    def abrir_conta(self, cpf_titular, saldo_inicial):
+
+    def fechar_conta(self, id_conta)
+##################################################
+# Ações
         
     def consultar_saldo(self, cpf):
         ...
@@ -38,8 +49,9 @@ class Banco:
         print('=' * 15, 'BANCO DO BONIN', '=' * 15)
         print('=' * 50)
         print()
-        print('1 - Acessar área do servidor')
-        print('2 - Acessar área do cliente' )
+        print('1 - Desejo ser cliente')
+        print('2 - Acessar área do servidor')
+        print('3 - Acessar área do cliente' )
         return int(input('Informe a seleção desejada: '))
         
     def menu_servidor(self):
@@ -67,3 +79,20 @@ class Banco:
         print('4 - Realizar transferência')
         print('0 - Voltar')
         return input('Digite a Ação requerida: ')
+
+    def menu_desejo_ser_cliente(self):
+        print('=' * 50)
+        print('=' * 15,'ÁREA DE CADASTRO', '=' * 15)
+        print('=' * 50)
+        return self.tornar_se_cliente()
+
+    def tornar_se_cliente(self):
+        nome = input('Digite o nome completo do cliente: ')
+        idade = int(input('Digite a idade do cliente: '))
+        cpf = int(input('Digite o CPF do cliente(Apenas número): '))
+
+        return nome, idade, cpf
+        
+
+
+
--- utils/banco_de_dados.py
+++ utils/banco_de_dados.py
@@ -5,3 +5,76 @@ import sqlite3
 
 conexao = sqlite3.connect('database/biblioteca.db')
 cursor = conexao.cursor()
+
+cursor.execute("""
+CREATE TABLE cliente IF IT NOT EXISTS(
+    id_cliente INTERGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    idade INTERGER NOT NULL,
+    cpf TEXT NOT NULL UNIQUE)
+""")
+
+cursor.execute("""
+CREATE TABLE conta IF IT NOT EXISTS( 
+    id_conta PRIMARY KEY AUTOINCREMENT,
+    cpf_titular TEXT NOT NULL,
+    saldo_inicial INTERGER NOT NULL
+
+FOREIGN KEY(cpf_titular) REFERENCES cliente_cpf(cpf))
+""")
+
+conexao.commit() 
+conexao.close()
+
+#########################################################
+
+def consultar_conta():
+    conexao = sqlite3.connect('database/biblioteca.db')
+    cursor = conexao.cursor()
+
+    # SELECT mostra as ifnromçãoes que queremos mostrar, FROM aponta a tabela principal
+    cursor.execute("""
+    SELECT cpf.cliente, conta.cpf_titular FROM cliente
+    JOIN conta
+        ON cliente.cpf = conta.cpf_titular
+    """)
+    # ON mostra a relação entre as duas colunas
+
+    consulta = cursor.fetchall()
+
+    for dado in consulta:
+        print(dado)
+
+    conexao.commit()
+    conexao.close()
+#################################################################
+# Cadastro
+
+def cadastro_cliente(nome, idade, cpf):
+    conexao = sqlite3.connect('database/biblioteca.db')
+    cursor = conexao.cursor()
+    
+    cursor.execute("""
+    INSERT INTO cliente(nome, idade, cpf)
+    VALUES( ?, ?, ?)
+    """, (nome, idade, cpf,))
+
+    conexao.commit()
+    conexao.close()
+
+def remover_cadastro(id_cliente):
+    conexao = sqlite3.connect('database/biblioteca.db')
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+    DELETE FROM clientes
+    WHERE id_cliente = ?
+    """, (id_cliente))
+
+    conexao.commit()
+    conexao.close()
+#################################################################
+def abertura_conta(cpf_titular, saldo_inicial):
+    ...
+
+def fechar_conta(id_conta)

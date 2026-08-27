5b71ab1154c3eefa6aba5749e5241444dee5301f
Daniel Bonin <danielbonin0011@gmail.com>
2026-08-27 13:48:02 -0300

    Finalização da versão 4.0, com .env, repository's, services, refatoração dos menus, refatoração de conexao_postgre e do main.py, como pontos de atuação.

--- database/conexao_postgre.py
+++ database/conexao_postgre.py
@@ -1,13 +1,16 @@
-import psycopg
+from dotenv import load_dotenv
+import psycopg, os
+
+load_dotenv()
+
 
 def conectar():
     return psycopg.connect(
-        dbname='projeto_banco',
-        user='postgres',
-        password='B@nin180506',
-        host='localhost',
-        port='5432',
-        connect_timeout=5
+        dbname=os.getenv('DB_NAME'),
+        user=os.getenv('BD_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        host=os.getenv('DB_HOST'),
+        port=os.getenv('DB_PORT'),
     )
 
 
@@ -15,12 +18,9 @@ print("Tentando conectar...")
 
 try:
     conexao = conectar()
-
     print("Conexão realizada com sucesso!")
-
     conexao.close()
 
 except Exception as erro:
     print("Erro ao conectar:")
     print(erro)
-conexao.close()
